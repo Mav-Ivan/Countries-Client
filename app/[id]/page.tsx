503a20da38a02ff0d1c00fce5038c5163c34714f
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2025-05-28 13:44:25 +0300

    added responsive view for country page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -36,41 +36,41 @@ export default function CountryPage() {
 
   return (
     <div className="px-10 bg-[#fafafa] dark:bg-[#222d37] min-h-screen">
-      <div className="h-35 flex items-center">
+      <div className="h-15 md:h-35 flex items-center">
         <Button variant="outline" onClick={handleClick}>
           <ArrowLeft /> Back
         </Button>
       </div>
-      <div className="flex gap-[15%] items-center">
+      <div className="flex flex-col md:flex-row gap-[15%] items-center">
         <Image
-          className="max-h-[400px] max-w-[600px]"
+          className="h-[30vh] w-[80vw]  lg:h-[40vh] lg:w-[40vw]"
           src={flag}
           width={600}
           height={600}
           alt="Nation flag"
         />
         <div>
-          <h1 className="font-bold text-4xl mb-15">{name}</h1>
+          <h1 className="font-bold text-3xl md:text-4xl my-7 md:mb-15">{name}</h1>
           <div className="flex gap-10">
             <div className="flex flex-col gap-5">
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Population: <span className="text-gray-500">{populationWithComas}</span>
               </p>
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Region: <span className="text-gray-500">{region}</span>
               </p>
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Sub Region: <span className="text-gray-500">{subregion}</span>
               </p>
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Capital: <span className="text-gray-500">{capital}</span>
               </p>
             </div>
             <div className="flex flex-col gap-5">
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Top Level Domain: <span className="text-gray-500">{topLevelDomain}</span>
               </p>
-              <p className="font-bold">
+              <p className="font-semibold lg:font-bold">
                 Languages:{" "}
                 {allLanguages.map((el, indx) => (
                   <span className="text-gray-500" key={uuidv4()}>
