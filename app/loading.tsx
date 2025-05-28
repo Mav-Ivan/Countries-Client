92d89b55d2befc67c846ac8977942c7fd761dbb4
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2025-05-28 22:26:33 +0300

    fixed height of loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 
 export default function Loading() {
   return (
-    <div className="flex h-[70vh] justify-center items-center bg-[#fafafa] dark:bg-[#222d37]">
+    <div className="flex h-[85vh] justify-center items-center bg-[#fafafa] dark:bg-[#222d37]">
       <ClipLoader size={50} color="#4A90E2" />
     </div>
   );
